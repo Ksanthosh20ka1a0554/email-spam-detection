75fc8f86c7d1b8684857cdac9134049ddc8dec7b
ksanthosh20ka1a0554 <ksanthosh20ka1a0554@gmail.com>
2023-06-22 13:56:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,render_template
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+import string
+
+def get_importantFeatures(sent):
+    sent = sent.lower()
+    return [i for i in nltk.word_tokenize(sent) if i.isalnum()]
+
+def remove_stopwords_and_punctuation(sent):
+    return [i for i in sent if i not in nltk.corpus.stopwords.words('english') and i not in string.punctuation]
+
+def perform_stemming(sent):
+    ps = nltk.PorterStemmer()
+    return [ps.stem(i) for i in sent]
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route("/review",methods=['GET','POST'])
+def predict():
+    if request.method=="POST":
+         test_message=request.form['message']
+         processed_message = perform_stemming(remove_stopwords_and_punctuation(get_importantFeatures(test_message)))
+         processed_message_str = ' '.join(processed_message)
+         with open('svm_model.pkl', 'rb') as file:
+            loaded_model = pickle.load(file)
+         with open('tfidf_vectorizer.pkl', 'rb') as file:
+            tfidf = pickle.load(file)
+         test_message_tfidf = tfidf.transform([processed_message_str])
+         prediction = loaded_model.predict(test_message_tfidf)
+    return render_template('index.html',final_result=prediction[0])
+
+# if __name__ == '__main__':
+#     app.run(debug = True)
