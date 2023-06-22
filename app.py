48e4f31760b2114031a7bd5c267da873aa0264f4
ksanthosh20ka1a0554 <ksanthosh20ka1a0554@gmail.com>
2023-06-22 22:50:50 +0530

    14 commit

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 from flask import Flask,request,render_template
 import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 import nltk
 import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 nltk.download('punkt')
 nltk.download('stopwords')
 
-
-with open('tfidf_vectorizer.pkl', 'rb') as file:
-    tfidf = pickle.load(file)
-
-with open('svm_model.pkl', 'rb') as file:
-    loaded_model = pickle.load(file)
-
-
-
-tfidf = joblib.load("tfidf_vectorizer.joblib")
+df = pd.read_csv('spam.csv',encoding = "ISO-8859-1")
 
 def get_importantFeatures(sent):
     sent = sent.lower()
@@ -27,6 +22,22 @@ def remove_stopwords_and_punctuation(sent):
 def perform_stemming(sent):
     ps = nltk.PorterStemmer()
     return [ps.stem(i) for i in sent]
+
+df['message'] = df['message'].apply(get_importantFeatures)
+df['message'] = df['message'].apply(remove_stopwords_and_punctuation)
+df['message'] = df['message'].apply(perform_stemming)
+
+X = df['message'].apply(lambda x: ' '.join(x))
+y = df['class']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+tfidf = TfidfVectorizer()
+X_train_tfidf = tfidf.fit_transform(X_train)
+
+X_test_tfidf = tfidf.transform(X_test)
+
+classifier = SVC(kernel='linear')
+classifier.fit(X_train_tfidf, y_train)
 
 app = Flask(__name__)
 @app.route('/')
@@ -40,8 +51,8 @@ def predict():
          processed_message = perform_stemming(remove_stopwords_and_punctuation(get_importantFeatures(test_message)))
          processed_message_str = ' '.join(processed_message)
          test_message_tfidf = tfidf.transform([processed_message_str])
-         prediction = loaded_model.predict(test_message_tfidf)
+         prediction = classifier.predict(test_message_tfidf)
     return render_template('index.html',final_result=prediction[0])
 
-# if __name__ == '__main__':
-#      app.run(debug = True)
+if __name__ == '__main__':
+     app.run(debug = True)
